023e9af2a826e0ff7c430319a3decf47afb1290a
dkatsal <katsal.dimon@gmail.com>
2021-03-16 14:07:27 +0200

    update mongoose Schema

--- Post.js
+++ Post.js
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose';
 
 const Post = new mongoose.Schema({
-  name: { type: String, required: false },
-  text: { type: String, required: false },
+  name: { type: String, required: true },
+  text: { type: String, required: true },
   time: { type: String, required: false },
 });
 
